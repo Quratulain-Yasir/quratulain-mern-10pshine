39614effa2bdac67da57fc1c8a42be8b5eb0f964
Quratulain-Yasir <hiraiqbal681@gmail.com>
2025-09-24 11:38:28 +0500

    connect DB with Nodejs app + create usermodel

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 // server.js
 
 import express from  "express" 
- 
+ import dotenv from "dotenv"
+ dotenv.config()
+ import connectDB from "./src/config/mongodb.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000
- 
+ connectDB()
 
 // Middleware
 app.use(express.json())
