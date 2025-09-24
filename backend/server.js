8728e124517b7ed6ce681a413ca2ffdbbf522613
Quratulain-Yasir <hiraiqbal681@gmail.com>
2025-09-24 08:32:35 +0500

    basic-backend-structure

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+// server.js
+
+const express = require("express")
+const app = express();
+const PORT = process.env.PORT || 5000
+
+// Middleware
+app.use(express.json())
+
+// Basic Route
+app.get("/" , (req , res)=> {
+    res.send("Server is Running.........")
+})
+
+// start server
+app.listen(PORT , ()=>{
+    console.log(`SERVER IS LISTENING ON PORT ${PORT}`)
+})
