15d68bb0b31c63d99ba428a3560b33d5556b39e3
Quratulain-Yasir <hiraiqbal681@gmail.com>
2025-09-26 13:09:55 +0500

    CRUD_API testing

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const userSchema = { 
-email:{ type:String , required:true} , 
-password:{ type:String , required:true , unique:true} , 
+email:{ type:String , required:true, unique:true} , 
+password:{ type:String , required:true} , 
 name:{ type:String , required:true}
 }
 
--- backend/src/routes/noteRoutes.js
+++ backend/src/routes/noteRoutes.js
@@ -1,13 +1,13 @@
 import express from "express"
 import { createNote , updateNote , deleteNote } from "../controllers/noteController.js"
-
+import auth from "../middleware/auth.js"
 
 const noteRoute = express.Router()
-noteRoute.post("/create" , createNote)
+noteRoute.post("/create" , auth , createNote)
 
-noteRoute.post("/update" , updateNote )
+noteRoute.post("/update/:id" , auth , updateNote )
  
- noteRoute.post("/delete" , deleteNote )
+ noteRoute.post("/delete/:id" , auth , deleteNote )
 
 
 export default noteRoute
