6c4c7d068cc7eaa1aa86fea9512b948fd01d2cfd
Quratulain-Yasir <hiraiqbal681@gmail.com>
2025-11-06 03:12:06 +0500

    user-profile and updateprofile api created

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,11 +1,16 @@
 import express from "express"
-import {registerUser , loginUser } from "../controllers/userController.js"
+import {registerUser , loginUser , getProfile , updateProfile } from "../controllers/userController.js"
+import auth from "../middleware/auth.js"
 
 
 const userRoute = express.Router()
 userRoute.post("/register" , registerUser )
 
 userRoute.post("/login" , loginUser )
+
+userRoute.get("/user-profile" , auth , getProfile )
+
+userRoute.post("/update-user-profile" , auth , updateProfile )
  
 
 
